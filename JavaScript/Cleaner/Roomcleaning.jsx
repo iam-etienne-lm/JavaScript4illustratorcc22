d65715ad95447c2ae36374a2b230cdca8cf7d4fb
ET <et3d@users.noreply.github.com>
2017-09-20 17:37:47 +0200

    Update Roomcleaning.jsx

--- JavaScript/Cleaner/Roomcleaning.jsx
+++ JavaScript/Cleaner/Roomcleaning.jsx
@@ -1,53 +1,54 @@
 #target Illustrator-21
 #script "ROOMCLEANING"
 "use strict"
+//app.userInteractionLevel = UserInteractionLevel.DISPLAYALERTS;
 $.gc();
 main();
-app.userInteractionLevel = UserInteractionLevel.DISPLAYALERTS;
-$.write("cleaned/saved")
 
 function main(){ //reverse order
-    
 	var docs=app.documents;
 	var i =docs.length-1;
-    var d
-
-app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS
-        for ( ; i >= 0; i-- ){
-         d=docs[i];
-       unselect();
-     unhide_Layer(d);
-    //saver(docs, i);  
-    $.write(" ,"+i);
-    }
+	//app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS
+     //unselect();
+     unhide_Layer();
+     //closer();
+    //saver();  
 }
 
 function unselect(){
         app.selection=null;
-    
 }
 
-function unhide_Layer(d){															// K LAYERS
+function unhide_Layer(){															// K LAYERS
 	var k=null;
-    var dl=d.layers;
-	//var doc_f1=null;
-	//doc_f1=app.activeDocument;
-	for ( k = 0; k < dl.length; k++ ){				//TODO set names instead of nb
-		dl[k].visible = true;
+	var doc_f1=null;
+	doc_f1=app.activeDocument;
+	for ( k = 0; k < doc_f1.layers.length; k++ ){				//TODO set names instead of nb
+		doc_f1.layers[k].visible = true;
 	}
 }
 
-function saver(docs, i){
-			
+function closer(){
+    for ( ; i >= 0; i-- ){
 		//var saveOptions = new IllustratorSaveOptions();  
 		//saveOptions.pdfCompatible = false;  
 		//d.save(d.fullname, saveOptions);  
 		//d.saved = true;
-		
-		if(docs[i].saved==false){
+		$.write(" i "+i);
+			docs[i].close(SaveOptions.DONOTSAVECHANGES);
+}
+
+function saver(){
+	for ( ; i >= 0; i-- ){		
+		//var saveOptions = new IllustratorSaveOptions();  
+		//saveOptions.pdfCompatible = false;  
+		//d.save(d.fullname, saveOptions);  
+		//d.saved = true;
+		$.write(" i "+i);
+		if(docs.saved==false){
 			docs[i].close(SaveOptions.SAVECHANGES);
 		}else{
 			docs[i].close(SaveOptions.DONOTSAVECHANGES);
-
+		}
 	}
 }
