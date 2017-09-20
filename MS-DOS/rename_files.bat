c391c2ebb45411cde026fdb31181f78e3aee89b1
ET <et3d@users.noreply.github.com>
2017-09-20 18:14:37 +0200

    Update rename_files.bat

--- MS-DOS/rename_files.bat
+++ MS-DOS/rename_files.bat
@@ -3,3 +3,8 @@ ren "DAO 312454*.pdf" "DAO 312818*.tmp"
 ren *.tmp *.pdf
 echo done
 pause
+
+::ELSE
+::cd /d "C:/TempAi/F5"
+::SET proj = 012345
+::for %%i in (*.pdf) do REN "%%i" "cad 012345-10A FORMATG-01-2%%~ni.tmp"
