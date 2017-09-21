1d5c93e216cdb9b5d44a7c13aadcf89f25fa57ef
ET <et3d@users.noreply.github.com>
2017-09-21 17:44:56 +0200

    Create text_edit.jsx

---
+++ JavaScript/Text_Manip/text_edit.jsx
@@ -0,0 +1,55 @@
+#target Illustrator-21
+#targetengine main
+$.gc();
+app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
+main();
+app.userInteractionLevel = UserInteractionLevel.DISPLAYALERTS;
+$.write("snr")
+
+function main(){ //reverse order
+    var d //= app.activeDocument
+    var docs=app.documents;
+    var i =docs.length-1;
+    for ( ; i >= 0; i-- ){
+        d=docs[i];
+        tgt();
+        $.write(" ,"+i);
+    }
+}
+
+function tgt(){
+     var t1, t2;
+    var d=app.activeDocument;
+    t1 = 'DAO ';
+    t2 = '' ;
+    cth_s(t1,t2);
+    t1 = 'FORMAT';
+    t2 = '              ';
+    cth_s(t1,t2);
+    t1 = '.pdf';
+    t2 = '';
+    cth_s(t1,t2);
+    }
+
+function cth_s(t1,t2){
+    //var openDocument = app.documents.item(0);  
+    var active_doc = app.activeDocument;
+    var search_string = t1; // g for global search, remove i to make a case sensitive search    
+        $.write(search_string)
+    var replace_string = t2;      
+    var text_frames = active_doc.textFrames;    
+        
+    if (text_frames.length > 0)    
+    {    
+        for (var i = 0 ; i < text_frames.length; i++)    
+          {    
+              var this_text_frame = text_frames[i];    
+               var new_string = this_text_frame.contents.replace(search_string, replace_string);    
+                   
+               if (new_string != this_text_frame.contents)    
+                   {    
+                        this_text_frame.contents = new_string;    
+                   }    
+          }    
+    }  
+}
