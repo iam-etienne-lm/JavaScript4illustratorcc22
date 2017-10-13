91ed8665fcd4ef16a8673aebba766c506eb3e4fc
ET <et3d@users.noreply.github.com>
2017-10-13 07:52:57 +0200

    Create dollar.js

---
+++ JavaScript/CMD/dollar.js
@@ -0,0 +1,47 @@
+// Dollar Class
+
+$.writeln("build: "+$.build);
+$.writeln("buildDate: "+$.buildDate);
+$.writeln("decimalPoint: "+$.decimalPoint);
+$.writeln("engineName: "+$.engineName);
+$.writeln("error: "+$.error);
+$.writeln("fileName: "+$.fileName);
+$.writeln("flags: "+$.flags);
+$.writeln("global: "+$.global);
+$.writeln("hiresTimer: "+$.hiresTimer);
+$.writeln("includePath: "+$.includePath);
+$.writeln("level: "+$.level);
+$.writeln("line: "+$.line);
+$.writeln("locale: "+$.locale);
+$.writeln("localize: "+$.localize);
+$.writeln("memCache: "+$.memCache);
+$.writeln("os: "+$.os);
+$.writeln("screens: "+$.screens);
+$.writeln("stack: "+$.stack);
+$.writeln("strict: "+$.strict);
+$.writeln("version: "+$.version);
+
+/**
+build: 80.1
+decimalPoint: ,
+buildDate: Wed Dec 03 2014 09:00:00 GMT+0100
+engineName: main
+error: Erreur
+fileName: (Script1)
+flags: 1024
+global: [object global]
+hiresTimer: 4567839213
+includePath: 
+level: 1
+line: 12
+locale: fr_FR
+localize: false
+memCache: 102400
+os: Windows 7/64 6.1 Service Pack 1 
+screens: 0:0-1920:1050,1920:0-3840:1080
+stack: [(Script1)]
+
+strict: false
+version: 4.5.6
+ 
+ */
