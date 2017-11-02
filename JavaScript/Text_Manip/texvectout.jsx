581b98ba5e0d433db1b2754e28ca60c43519a432
ET <et3d@users.noreply.github.com>
2017-11-02 18:23:22 +0100

    Create texvectout.jsx

---
+++ JavaScript/Text_Manip/texvectout.jsx
@@ -0,0 +1,28 @@
+#target Illustrator-21
+#script "texvectout"
+"use strict"
+$.level=2;
+tvect();
+$.write("stackPrint sing ok >");
+$.gc();
+
+function tvect(){
+    var d= app.activeDocument;
+    var tf= d.layers['trace'].textFrames;
+    var i = tf.length-1;
+        for ( ; i>=0 ; i--){
+            tf[i].selected=true;
+            d.selection[0].createOutline();
+            app.executeMenuCommand('Live Pathfinder Outline');
+        }
+    }
+
+function selgp(){
+    var d= app.activeDocument;
+    var g = d.layers['trace'].groupItems;
+    var i = g.length-1;
+    for ( ; i>=0 ; i--){
+        g[i].selected=true;
+        app.executeMenuCommand('Live Pathfinder Outline');
+    }
+}
