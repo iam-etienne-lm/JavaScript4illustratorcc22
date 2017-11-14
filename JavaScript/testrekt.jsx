fbe2e86dcb06e880c16d2948291792f4e6179397
ET <et3d@users.noreply.github.com>
2017-11-14 20:09:56 +0100

    Create testrekt.jsx

---
+++ JavaScript/testrekt.jsx
@@ -0,0 +1,18 @@
+#target Illustrator-22
+#script "rekt"
+"use strict"
+main();
+$.write("rekt")
+$.gc();
+
+function main(){
+    var d,
+    x,
+    y,
+    bot,
+    rig;
+    
+    d=app.activeDocument;
+    d.pathItems.rectangle(x,y,10,10);
+    
+}
