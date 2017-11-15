0c3e29691ff28b5a86cbb9f4406f240d393d302e
ET <et3d@users.noreply.github.com>
2017-11-15 20:11:54 +0100

    Update testrekt.jsx

--- JavaScript/testrekt.jsx
+++ JavaScript/testrekt.jsx
@@ -10,13 +10,21 @@ function main(){
     i,
     x,
     y,
+    ab,
     bot,
     rig;
     
     d=app.activeDocument;
+    d.pathItems.rectangle(-20,0,20,10);
+    
+    /*a = d.Artboards[0];
+    ab=d.Artboards[0].geometricBounds;
+    bot = ab[3];
+    rig = ab[2];
+    
     for (i=0;i<100;i++){
         x=i%rig;
         y=i%bot;
         d.pathItems.rectangle(x,y,10,10);
-    }
+    }*/
 }
